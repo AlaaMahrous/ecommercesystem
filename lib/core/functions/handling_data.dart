4e86eea66fb17b781c7c39804119dcac2762af54
Alaa Mahrous <alamhrws8@gmail.com>
2026-07-16 22:22:35 +0300

    handling data

---
+++ lib/core/functions/handling_data.dart
@@ -0,0 +1,9 @@
+import 'package:ecommercesystem/core/class/status_request.dart';
+
+StatusRequest handlingData(dynamic response) {
+  if (response is StatusRequest) {
+    return response;
+  } else {
+    return StatusRequest.success;
+  }
+}
