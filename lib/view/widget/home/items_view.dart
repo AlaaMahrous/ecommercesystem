1c3e83085c56b42bcedbf79a410a260db2823cca
Alaa Mahrous <alamhrws8@gmail.com>
2026-08-26 01:42:40 +0300

    items view

--- lib/view/widget/home/items_view.dart
+++ lib/view/widget/home/items_view.dart
@@ -14,11 +14,9 @@ class ItemsView extends StatelessWidget {
       onTap: () {},
       child: SizedBox(
         height: 250,
-
         child: ListView.builder(
           scrollDirection: Axis.horizontal,
           itemCount: items.length,
-
           itemBuilder: (context, index) {
             final item = items[index];
 
@@ -27,14 +25,10 @@ class ItemsView extends StatelessWidget {
 
             return Container(
               width: 160,
-
-              margin: const EdgeInsets.only(right: 15, left: 5),
-
+              margin: const EdgeInsets.only(right: 13, left: 10),
               decoration: BoxDecoration(
                 color: Colors.white,
-
                 borderRadius: BorderRadius.circular(18),
-
                 boxShadow: [
                   BoxShadow(
                     color: Colors.grey.withValues(alpha: 0.15),
@@ -43,40 +37,35 @@ class ItemsView extends StatelessWidget {
                   ),
                 ],
               ),
-
               child: Padding(
                 padding: const EdgeInsets.all(10),
-
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Stack(
                       children: [
-                        Container(
-                          height: 120,
-                          width: double.infinity,
-
-                          decoration: BoxDecoration(
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(15),
+                          child: Container(
+                            height: 120,
+                            width: double.infinity,
                             color: Colors.grey.shade100,
-
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-
-                          child: Padding(
-                            padding: const EdgeInsets.all(8),
-
-                            child: Image.asset(
-                              '${AppImages.imagesRoot}/${item.itemImage}',
-
-                              fit: BoxFit.contain,
-
-                              errorBuilder: (context, error, stackTrace) {
-                                return const Icon(
-                                  Icons.image,
-                                  size: 40,
-                                  color: Colors.grey,
-                                );
-                              },
+                            child: Padding(
+                              padding: const EdgeInsets.all(8),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(12),
+                                child: Image.asset(
+                                  '${AppImages.imagesRoot}/${item.itemImage}',
+                                  fit: BoxFit.contain,
+                                  errorBuilder: (context, error, stackTrace) {
+                                    return const Icon(
+                                      Icons.image,
+                                      size: 40,
+                                      color: Colors.grey,
+                                    );
+                                  },
+                                ),
+                              ),
                             ),
                           ),
                         ),
@@ -85,21 +74,17 @@ class ItemsView extends StatelessWidget {
                           Positioned(
                             top: 8,
                             left: 6,
-
                             child: Container(
                               padding: const EdgeInsets.symmetric(
                                 horizontal: 8,
                                 vertical: 4,
                               ),
-
                               decoration: BoxDecoration(
                                 color: Colors.red,
                                 borderRadius: BorderRadius.circular(8),
                               ),
-
                               child: Text(
                                 "-${item.itemDiscount}%",
-
                                 style: const TextStyle(
                                   color: Colors.white,
                                   fontSize: 11,
@@ -111,15 +96,12 @@ class ItemsView extends StatelessWidget {
                       ],
                     ),
 
-                    const SizedBox(height: 10),
+                    const SizedBox(height: 11),
 
                     Text(
                       item.itemName,
-
                       maxLines: 1,
-
                       overflow: TextOverflow.ellipsis,
-
                       style: const TextStyle(
                         fontSize: 14,
                         fontWeight: FontWeight.bold,
@@ -133,11 +115,9 @@ class ItemsView extends StatelessWidget {
                         if (item.itemDiscount > 0)
                           Text(
                             "${item.itemPrice} \$",
-
                             style: TextStyle(
                               fontSize: 12,
                               color: Colors.grey.shade500,
-
                               decoration: TextDecoration.lineThrough,
                             ),
                           ),
@@ -146,7 +126,6 @@ class ItemsView extends StatelessWidget {
 
                         Text(
                           "${newPrice.toStringAsFixed(0)} \$",
-
                           style: const TextStyle(
                             fontSize: 15,
                             fontWeight: FontWeight.bold,
@@ -159,16 +138,13 @@ class ItemsView extends StatelessWidget {
 
                     Align(
                       alignment: Alignment.bottomRight,
-
                       child: Container(
                         width: 35,
                         height: 35,
-
                         decoration: const BoxDecoration(
                           shape: BoxShape.circle,
                           color: AppColours.pPurple,
                         ),
-
                         child: const Icon(Icons.add, color: Colors.white),
                       ),
                     ),
